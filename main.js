e3e617a55d68062de6cadda0e62b3b5a8a9f9041
SeaCrap <506076395@qq.com>
2018-10-15 20:13:10 +0800

    升级 jQuery.ajax && 使用 Promise 配置

--- main.js
+++ main.js
@@ -1,31 +1,40 @@
-window.jQuery = function(){}
 
-window.$ = window.jQuery
 
-window.jQuery.ajax = function({url,method,body,success, fail}){
-  let request = new XMLHttpRequest
-  request.open(method,url)
-  request.send(body)
+window.jQuery.ajax = function({url,method,body}){
+  return new Promise(function(resove,reject){
+    let request = new XMLHttpRequest
+    request.open(method,url)
+    request.send(body)
   
-  request.onreadystatechange = ()=>{
-    if(request.readyState === 4){
-      //请求相应完毕
-      if(request.status <= 200&& request.status < 300){
-        //成功
-        success.call(undefined,request.responseText)
-      }else if(request.status >= 400){
-        //失败
-        fail.call(undefined,request)
+    request.onreadystatechange = ()=>{
+      if(request.readyState === 4){
+        //请求相应完毕
+        if(request.status <= 200&& request.status < 300){
+          //成功
+          resove.call(undefined,request.responseText)
+        }else if(request.status >= 400){
+          //失败
+          reject.call(undefined,request)
+        }
       }
-    }
-  }  
+    }    
+  })
+  
 }
 
 button.addEventListener('click',(e)=>{
-  window.jQuery.ajax({
+  
+  $.ajax({
     url: '/zzz',
     method: 'post',
     success: (responseText)=>{console.log(responseText)},
     fail: (request)=>{request}
-  })
+  }).them(
+    (text)=>{
+      console.log(text)
+    },
+    (request)=>{
+      console.log(request)
+    }
+  )
 })
